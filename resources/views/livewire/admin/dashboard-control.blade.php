b45843b828478269a44435fa632fd583cd00da1f
Rafie <rafiekom@gmail.com>
2025-07-13 16:12:26 +0800

    ddddd

--- resources/views/livewire/admin/dashboard-control.blade.php
+++ resources/views/livewire/admin/dashboard-control.blade.php
@@ -1,7 +1,9 @@
 <div>
     <div class="container-fluid px-4">
         <h1 class="mt-4">Dashboard</h1>
-      
+        <ol class="breadcrumb mb-4">
+            <li class="breadcrumb-item active">Dashboard Control</li>
+        </ol>
         </ol>
         <div class="row">
             <div class="col-xl-4 col-md-6">
